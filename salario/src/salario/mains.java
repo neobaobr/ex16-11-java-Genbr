fa1f8996d29e07fa11ca0fb1627227cdda45c3c3
neobaobr <36973680+neobaobr@users.noreply.github.com>
2020-11-16 17:07:04 -0300

    Update mains.java

--- salario/src/salario/mains.java
+++ salario/src/salario/mains.java
@@ -5,18 +5,23 @@ import java.util.Scanner;
 public class mains {
 
 	public static void main(String[] args) {
-		int horas, salario = 0, extra = 0, total;
+		int horas, salario = 0, extra = 0, total,codigo;
 		Scanner scan = new Scanner(System.in);
-		System.out.println("Digite o numero de horas trabalhadas");
+		System.out.println("Digite o numero de horas trabalhadas \n");	
 		horas = scan.nextInt();
+		System.out.println("Digite o numero de codigo de trabalhor \n");
+		codigo= scan.nextInt();
+
 		salario = 10 * horas;
 		extra = (horas - 50) * 20;
 
 		if (horas > 50) {
 			total = salario + extra - (horas - 50) * 10;
+			System.out.println("codigo do trabalhador\n"+codigo);
 			System.out.println("salario \n" + total);
 		} else {
 			extra = 0;
+			System.out.println("codigo do trabalhador\n"+codigo);
 			System.out.println("Salario \n");
 			System.out.println(+salario);
 		}
@@ -24,3 +29,4 @@ public class mains {
 	}
 
 }
+
